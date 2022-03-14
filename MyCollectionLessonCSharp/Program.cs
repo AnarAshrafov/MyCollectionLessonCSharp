e7d7666607497f8ab3a887c9c4f630d496331e8f
Anar Ashrafov <aa.it.devops@gmail.com>
2022-03-14 12:18:24 +0100

    Add project files.

---
+++ MyCollectionLessonCSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace MyCollectionLessonCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArr = new int[6];
+            numArr[0] = 10;
+            //numArr[2] = 20;
+            numArr[1] = 30;
+            numArr[3] = 40;
+            numArr[4] = 50;
+            numArr[5] = 60;
+            Array.Resize(ref numArr,10);
+
+            WriteLine();
+            foreach (var num in numArr)
+            {
+                Write($" | {numArr[num]}");
+            }
+
+            for (int i = 0;i < numArr.Length;i++)
+            {
+                Write($" | {numArr[i]}");
+            }
+
+            Write(" |\n");
+
+            ReadKey();
+        }
+       
+    }
+}
